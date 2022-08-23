47256a81b5638a4e17a95ee885f46fbeeac421b9
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-08-23 17:08:06 -0500

    added questions

--- index.js
+++ index.js
@@ -4,7 +4,54 @@ const fs = require("fs");
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please describe your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Who contributed to this project?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Outline the steps to use this project',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Please outline any installation requirements',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Enter any tests for this project',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    },
+    {
+        type: 'list',
+        message: 'Please choose a license for this project:',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0', 'Mozilla Public License 2.0', 'None']
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
